1d8b9b4cdeee2d4f03c8fe11e70e8892bddbea42
Dusty Phillips <buchuki@gmail.com>
2009-05-27 23:53:23 -0400

    Add a basic setup script.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import opterator
+
+setup(
+        name = "opterator",
+        version = opterator.__version__,
+        py_modules = ['opterator'],
+        author = "Dusty Phillips",
+        author_email = "dusty@linux.ca",
+        license = "MIT",
+        keywords = "opterator option parse parser options",
+        url = "http://github.com/buchuki/opterator/",
+        long_description = """At heart, a decorator for a script's main
+entry point that uses a function function signature and docstring to
+pseudo-automatically create an option parser. When invoked, the option parser
+automatically maps command-line arguments to function parameters."""
+        )
