f6d5352e502e498cc68638ca050a6ddeb72bdec0
Dusty Phillips <buchuki@gmail.com>
2009-05-27 23:58:44 -0400

    Use ez_setup with the script

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import ez_setup
+ez_setup.use_setuptools()
 from setuptools import setup
 import opterator
 
@@ -13,5 +15,6 @@ setup(
         long_description = """At heart, a decorator for a script's main
 entry point that uses a function function signature and docstring to
 pseudo-automatically create an option parser. When invoked, the option parser
-automatically maps command-line arguments to function parameters."""
+automatically maps command-line arguments to function parameters.""",
+        download_url = "http://cloud.github.com/downloads/buchuki/opterator/opterator-0.1.tar.gz"
         )
