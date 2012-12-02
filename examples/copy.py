6012a332baf140ca5521c2288642a7d1d1a942a6
Dusty Phillips <dusty@linux.ca>
2012-12-01 21:16:31 -0700

    update the example

--- examples/copy.py
+++ examples/copy.py
@@ -1,24 +1,26 @@
 from opterator import opterate
+
+
 @opterate
 def main(filename1, filename2, recursive=False, backup=False,
         suffix='~', *other_filenames):
     '''An example copy script with some example parameters that might
     be used in a copy command.
-    
-    @param recursive -r --recursive copy directories
+
+    :param recursive: -r --recursive copy directories
         recursively
-    @param backup -b --backup backup any files you copy over
-    @param suffix -S --suffix override the usual backup
+    :param backup: -b --backup backup any files you copy over
+    :param suffix: -S --suffix override the usual backup
         suffix '''
     filenames = [filename1, filename2] + list(other_filenames)
     destination = filenames.pop()
 
-    print "You asked to move %s to %s" % (filenames, destination)
+    print("You asked to move %s to %s" % (filenames, destination))
     if recursive:
-        print "You asked to copy directories recursively."
+        print("You asked to copy directories recursively.")
     if backup:
-        print "You asked to backup any overwritten files."
-        print "You would use the suffix %s" % suffix
+        print("You asked to backup any overwritten files.")
+        print("You would use the suffix %s" % suffix)
 
 if __name__ == '__main__':
     main()
