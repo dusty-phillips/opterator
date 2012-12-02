48cee20c13855ecc83ba2808b8347a45b578df21
Dusty Phillips <dusty@linux.ca>
2012-12-01 21:06:42 -0700

    pep8

--- opterator_test.py
+++ opterator_test.py
@@ -8,19 +8,23 @@ class Checker(object):
     correctly.'''
     pass
 
+
 def test_noargs():
     '''Ensure a method that expects no arguments runs properly when it receives
     none.'''
     result = Checker()
+
     @opterate
     def main():
         result.x = 5
     main([])
     assert result.x == 5
 
+
 def test_noarg_help():
     '''assert that a help message is shown when -h is passed.'''
     capture = py.io.StdCapture()
+
     @opterate
     def main():
         '''a docstring'''
@@ -35,9 +39,11 @@ a docstring
 Options:
   -h, --help  show this help message and exit"""
 
+
 def test_noarg_invalid():
     '''Ensure that passing an illegal argument is caught as such.'''
     capture = py.io.StdCapture()
+
     @opterate
     def main():
         '''a docstring'''
@@ -46,12 +52,14 @@ def test_noarg_invalid():
     out, error = capture.reset()
     assert out == ''
     assert error.strip() == ('Usage: py.test [options]'
-            '\n\na docstring\n\n'
-            'py.test: error: no such option: --invalidopt')
+                             '\n\na docstring\n\n'
+                             'py.test: error: no such option: --invalidopt')
+
 
 def test_noarg_param():
     '''Ensure that passing a parameter is rejected.'''
     capture = py.io.StdCapture()
+
     @opterate
     def main():
         '''a docstring'''
@@ -60,12 +68,14 @@ def test_noarg_param():
     out, error = capture.reset()
     assert out == ''
     assert error.strip() == ('Usage: py.test [options]'
-            '\n\na docstring\n\n'
-            'py.test: error: Too many arguments.')
+                             '\n\na docstring\n\n'
+                             'py.test: error: Too many arguments.')
+
 
 def test_var_positional_noopts():
     '''Try passing a list of positional arguments to the function.'''
     result = Checker()
+
     @opterate
     def main(*filenames):
         '''list files in a directory.'''
@@ -74,22 +84,26 @@ def test_var_positional_noopts():
     main(filenames)
     assert list(result.filenames) == filenames
 
+
 def test_var_positional_help():
     capture = py.io.StdCapture()
+
     @opterate
     def main(*filenames):
         'list files'
         pass
     py.test.raises(SystemExit, main,
-            ['--help'])
+                   ['--help'])
     out, error = capture.reset()
     assert error == ''
     assert out.strip() == ('Usage: py.test [options] [filenames]'
-            '\n\nlist files\n\n'
-            'Options:\n  -h, --help  show this help message and exit')
+                           '\n\nlist files\n\n'
+                           'Options:\n  -h, --help  show this help message and exit')
+
 
 def test_required_positional():
     result = Checker()
+
     @opterate
     def main(source, dest):
         'move a file'
@@ -99,36 +113,42 @@ def test_required_positional():
     assert result.source == 'sourcename'
     assert result.dest == 'destname'
 
+
 def test_too_many_positional():
     capture = py.io.StdCapture()
+
     @opterate
     def main(source, dest):
         'copy a file'
         pass
     py.test.raises(SystemExit, main,
-            ['sourcename', 'destname', 'somethingextra'])
+                   ['sourcename', 'destname', 'somethingextra'])
     out, error = capture.reset()
     assert out == ''
     assert error.strip() == ('Usage: py.test [options] source dest'
-            '\n\ncopy a file\n\n'
-            'py.test: error: Too many arguments.')
+                             '\n\ncopy a file\n\n'
+                             'py.test: error: Too many arguments.')
+
 
 def test_not_enough_positional():
     capture = py.io.StdCapture()
+
     @opterate
     def main(source, dest):
         'copy a file'
         pass
     py.test.raises(SystemExit, main,
-            ['sourcename'])
+                   ['sourcename'])
     out, error = capture.reset()
     assert out == ''
     assert error.strip() == ('Usage: py.test [options] source dest'
-            '\n\ncopy a file\n\n'
-            'py.test: error: Not enough arguments.')
+                             '\n\ncopy a file\n\n'
+                             'py.test: error: Not enough arguments.')
+
 
 def test_keyword_option():
     result = Checker()
+
     @opterate
     def main(myoption='novalue'):
         '''A script with one optional option.
@@ -138,13 +158,15 @@ def test_keyword_option():
     main(['-m', 'avalue'])
     assert result.myoption == 'avalue'
 
+
 def test_keyword_option_helptext():
     capture = py.io.StdCapture()
+
     @opterate
     def main(myoption='novalue'):
         '''A script with one optional option.
         @param myoption -m --mine the myoption helptext'''
-        result.myoption = myoption
+        print("never called")
     py.test.raises(SystemExit, main, ['-h'])
     out, error = capture.reset()
     print(out)
@@ -158,8 +180,10 @@ Options:
   -m MYOPTION, --mine=MYOPTION
                         the myoption helptext"""
 
+
 def test_short_option_only():
     result = Checker()
+
     @opterate
     def main(myoption='novalue'):
         '''A script with one optional option.
@@ -169,8 +193,10 @@ def test_short_option_only():
     main(['-m', 'avalue'])
     assert result.myoption == 'avalue'
 
+
 def test_long_option_only():
     result = Checker()
+
     @opterate
     def main(myoption='novalue'):
         '''A script with one optional option.
@@ -179,9 +205,11 @@ def test_long_option_only():
 
     main(['--mine', 'avalue'])
     assert result.myoption == 'avalue'
-    
+
+
 def test_keyword_option_is_optional():
     result = Checker()
+
     @opterate
     def main(myoption='novalue'):
         '''A script with one optional option.
@@ -191,8 +219,10 @@ def test_keyword_option_is_optional():
     main([])
     assert result.myoption == 'novalue'
 
+
 def test_keyword_list_option():
     result = Checker()
+
     @opterate
     def main(myoption=[]):
         '''A script with one optional option that can be repeated.
@@ -202,8 +232,10 @@ def test_keyword_list_option():
     main(['-m', 'hi'])
     assert result.myoption == ['hi']
 
+
 def test_keyword_list_multiple_options():
     result = Checker()
+
     @opterate
     def main(myoption=[]):
         '''A script with one optional option that can be repeated.
@@ -213,8 +245,10 @@ def test_keyword_list_multiple_options():
     main(['-m', 'hi', '-m', 'yo'])
     assert result.myoption == ['hi', 'yo']
 
+
 def test_keyword_list_no_options():
     result = Checker()
+
     @opterate
     def main(myoption=[]):
         '''A script with one optional option that can be repeated.
@@ -224,8 +258,10 @@ def test_keyword_list_no_options():
     main([])
     assert result.myoption == []
 
+
 def test_required_arg_kw_option():
     result = Checker()
+
     @opterate
     def main(firstarg, myoption='novalue'):
         '''A script with one required argument and one optional value.
@@ -237,8 +273,10 @@ def test_required_arg_kw_option():
     assert result.myoption == 'avalue'
     assert result.firstarg == 'thearg'
 
+
 def test_required_arg_kw_option_is_optional():
     result = Checker()
+
     @opterate
     def main(firstarg, myoption='novalue'):
         '''A script with one required argument and one optional value.
@@ -250,8 +288,10 @@ def test_required_arg_kw_option_is_optional():
     assert result.myoption == 'novalue'
     assert result.firstarg == 'thearg'
 
+
 def test_varargs_kw_option():
     result = Checker()
+
     @opterate
     def main(myoption='novalue', *someargs):
         '''A script with one required argument and one optional value.
@@ -263,8 +303,10 @@ def test_varargs_kw_option():
     assert result.myoption == 'avalue'
     assert result.someargs == ('anarg', 'thearg')
 
+
 def test_varargs_kw_option_is_optional():
     result = Checker()
+
     @opterate
     def main(myoption='novalue', *someargs):
         '''A script with one required argument and one optional value.
@@ -276,8 +318,10 @@ def test_varargs_kw_option_is_optional():
     assert result.myoption == 'novalue'
     assert result.someargs == ('anarg', 'thearg')
 
+
 def test_two_kw_options():
     result = Checker()
+
     @opterate
     def main(myoption='novalue', secondoption=False):
         '''A script with one required argument and one optional value.
@@ -290,15 +334,17 @@ def test_two_kw_options():
     assert result.myoption == 'avalue'
     assert result.secondoption == True
 
+
 def test_comprehensive_example():
     result = Checker()
+
     @opterate
     def main(filename1, filename2, recursive=False, interactive=False,
-            suffix='~', *other_filenames):
+             suffix='~', *other_filenames):
         '''An example copy script with some example parameters borrowed from
         the cp man page. Illustrates some of the simplicity and pitfalls of
         this option parsing method.
-        
+
         @param recursive -r --recursive copy directories
             recursively
         @param interactive -i --interactive prompt before
@@ -308,10 +354,10 @@ def test_comprehensive_example():
         # When two filenames are required and others optional you have to build
         # funny lists like this.
         filenames = [filename1, filename2] + list(other_filenames)
-        destination = filenames.pop()
+        filenames.pop()
         # call function that does the actual checking, copying, like with any
         # option parsed app.
-        # 
+        #
         # ...
         result.filename1 = filename1
         result.filename2 = filename2
@@ -338,7 +384,6 @@ Options:
   -i, --interactive     prompt before overwrite
   -S SUFFIX, --suffix=SUFFIX
                         override the usual backup suffix'''
-
 
     main(['source', 'dest'])
     assert result.filename1 == 'source'
