8ef57e97849c9caf982dfd2067e8b795138ac90c
Dusty Phillips <dusty@linux.ca>
2012-12-01 21:17:21 -0700

    pep8 setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 import opterator
 
 setup(
-        name = "opterator",
-        version = opterator.__version__,
-        py_modules = ['opterator'],
-        author = "Dusty Phillips",
-        author_email = "dusty@linux.ca",
-        license = "MIT",
-        keywords = "opterator option parse parser options",
-        url = "http://github.com/buchuki/opterator/",
-        description = "Easy option parsing introspected from function signature.",
-        long_description = """At heart, a decorator for a script's main
+        name="opterator",
+        version=opterator.__version__,
+        py_modules=['opterator'],
+        author="Dusty Phillips",
+        author_email="dusty@linux.ca",
+        license="MIT",
+        keywords="opterator option parse parser options",
+        url="http://github.com/buchuki/opterator/",
+        description="Easy option parsing introspected from function signature.",
+        long_description="""At heart, a decorator for a script's main
 entry point that uses a function function signature and docstring to
 pseudo-automatically create an option parser. When invoked, the option parser
 automatically maps command-line arguments to function parameters.""",
-        download_url = "http://cloud.github.com/downloads/buchuki/opterator/opterator-0.1.tar.gz"
+        download_url="http://cloud.github.com/downloads/buchuki/opterator/opterator-0.1.tar.gz"
         )
