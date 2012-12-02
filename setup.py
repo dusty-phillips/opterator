366becc176c1da835569f1b15ad1fac7cbb36325
Dusty Phillips <dusty@linux.ca>
2012-12-01 21:20:31 -0700

    tweak setup

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
         keywords="opterator option parse parser options",
         url="http://github.com/buchuki/opterator/",
         description="Easy option parsing introspected from function signature.",
-        long_description="""At heart, a decorator for a script's main
-entry point that uses a function function signature and docstring to
+        long_description="""A decorator for a script's main
+entry point that uses a function signature and docstring to
 pseudo-automatically create an option parser. When invoked, the option parser
 automatically maps command-line arguments to function parameters.""",
         download_url="http://cloud.github.com/downloads/buchuki/opterator/opterator-0.1.tar.gz"
