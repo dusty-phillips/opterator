75d685b6e642b47163c7044756dad4f759c5c1ec
Dusty Phillips <dusty@buchuki.com>
2015-01-08 20:39:48 -0800

    Update classifiers and pep8

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from setuptools import setup
 import opterator
 
 setup(
-        name="opterator",
-        version=opterator.__version__,
-        py_modules=['opterator', 'test_opterator'],
-        author="Dusty Phillips",
-        author_email="dusty@buchuki.com",
-        license="MIT",
-        keywords="opterator option parse parser options",
-        url="http://github.com/buchuki/opterator/",
-        description="Easy option parsing introspected from function signature.",
-        download_url="https://github.com/buchuki/opterator/archive/%s.tar.gz" % opterator.__version__,
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
-        ]
-    )
+    name="opterator",
+    version=opterator.__version__,
+    py_modules=['opterator', 'test_opterator'],
+    author="Dusty Phillips",
+    author_email="dusty@buchuki.com",
+    license="MIT",
+    keywords="opterator option parse parser options",
+    url="http://github.com/buchuki/opterator/",
+    description="Easy option parsing introspected from function signature.",
+    download_url="https://github.com/buchuki/opterator/archive/%s.tar.gz" % opterator.__version__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
